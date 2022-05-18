f8e83962bb110d9180c9f29787ef9914ab491b00
ruchitpatel2002 <49008776+ruchitpatel2002@users.noreply.github.com>
2022-05-18 12:13:48 -0400

    Testing for fmt_pct

--- tests/testthat/test-test-independent-testing-fmt_pct.R
+++ tests/testthat/test-test-independent-testing-fmt_pct.R
@@ -1,10 +1,21 @@
 test_that("Test different type of Numeric Vectors and Digits", {
-  #Test various numeric inputs, digits combination and bracket type
-  #Test when input is NA
+  expect_equal(fmt_pct(NA),NA)
+
+  expect_equal(fmt_pct(12),"(12.0)")
+  expect_equal(fmt_pct(12.3),"(12.3)")
+  expect_equal(fmt_pct(12.343,digits = 2),"(12.34)")
+  expect_equal(fmt_pct(12.345,digits = 2),"(12.35)")
+  expect_equal(fmt_pct(12.345,digits = 0),"(12)")
+
+  expect_equal(fmt_pct(-12.345,digits = 0),"(-12)")
+  expect_equal(fmt_pct(-12.345,digits = 2),"(-12.35)")
+
 })
 
 test_that("Test on data frame column", {
-  #Create data frame column with various value.
-  #Include NA as one of the value
-  #check if the output column values are as expected
+
+  dt <- data.frame(col1 =c(0.01, 1.23, 12.34, 123.456, NA))
+  #For df column, length of each string be equal to max length of input column.
+  expect_equal(fmt_pct(dt$col1), c("  (0.0)", "  (1.2)", " (12.3)", "(123.5)", NA)  )
+
 })
