70369eadab147bf7b7c55dafa6c28cca86a6978b
baekhow <howard.baek@merck.com>
2022-07-27 09:36:38 -0400

    initial commit

--- tests/testthat/test-independent-testing-tlf_ae_summary.R
+++ tests/testthat/test-independent-testing-tlf_ae_summary.R
@@ -1,25 +1,31 @@
 library(metalite)
 library(metalite.ae)
 
-tempdir()
 meta <- meta_ae_dummy()
 outdata <- prepare_ae_summary(meta,
                               population = "apat",
                               observation = "wk12",
                               parameter = "any;rel;ser")
 tbl <- outdata |>
-  format_ae_summary()
-
-tbl |>
-  tlf_ae_summary(
-    source = "Source:  [CDISCpilot: adam-adsl; adae]",
-    path_outdata = paste0(tempdir(),"/aesummary2.rdata"),
-    path_outtable = paste0(tempdir(),"/aesummary2.rtf"))
+  format_ae_summary() |>
+                        tlf_ae_summary(
+                           source = "Source:  [CDISCpilot: adam-adsl; adae]",
+                           path_outtable = "./tests/testthat/fixtures/aesummary2.rtf",
+                           path_outdata = "./tests/testthat/fixtures/aesummary2.rdata")
 
 test_that("aesummary.rtf file output", {
-  expect_snapshot_file(paste0(tempdir(),"/aesummary2.rtf"))
+  # expect_snapshot_file(tbl |>
+  #                        tlf_ae_summary(
+  #                          source = "Source:  [CDISCpilot: adam-adsl; adae]",
+  #                          path_outtable = "./tests/testthat/fixtures/aesummary2.rtf"),
+  #                      "./tests/testthat/fixtures/aesummary2.rtf")
+  expect_snapshot_file("./tests/testthat/fixtures/aesummary2.rtf")
 })
-
-test_that("aesummary.rdata file output", {
-  expect_snapshot_file(paste0(tempdir(),"/aesummary2.rdata"))
-})
+#
+# test_that("aesummary.rdata file output", {
+#   expect_snapshot_file(tbl |>
+#                          tlf_ae_summary(
+#                            source = "Source:  [CDISCpilot: adam-adsl; adae]",
+#                            path_outdata = "./tests/testthat/fixtures/aesummary2.rdata"),
+#                        "./tests/testthat/fixtures/aesummary2.rdata")
+# })
