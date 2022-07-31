d9415b043f23b5e4ac4e6ebc2dc36479d14274b1
baekhow <howard.baek@merck.com>
2022-07-31 19:20:02 -0400

    rerun Ben's code

--- tests/testthat/test-independent-testing-tlf_ae_summary.R
+++ tests/testthat/test-independent-testing-tlf_ae_summary.R
@@ -19,14 +19,3 @@ test_that("aesummary.rtf file output", {
   expect_snapshot_file(file.path(testthat::test_path(),'fixtures', 'aesummary2.rtf'))
   expect_snapshot_file(file.path(testthat::test_path(),'fixtures', 'aesummary2.rdata'))
 })
-
-
-#
-# test_that("aesummary.rdata file output", {
-#   expect_snapshot_file(tbl |>
-#                          tlf_ae_summary(
-#                            source = "Source:  [CDISCpilot: adam-adsl; adae]",
-#                            path_outdata = "./tests/testthat/fixtures/aesummary2.rdata"),
-#                        "./tests/testthat/fixtures/aesummary2.rdata")
-# })
-
