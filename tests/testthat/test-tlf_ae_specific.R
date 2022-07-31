c41b50c72773b91156a6fee5a742ee34c4047c93
baekhow <howard.baek@merck.com>
2022-07-31 19:50:57 -0400

    snapshot testing for tlf_ae_specific()

--- tests/testthat/test-tlf_ae_specific.R
+++ tests/testthat/test-tlf_ae_specific.R
@@ -1,13 +1,21 @@
+library(metalite)
+
 meta <- meta_ae_dummy()
 outdata <- prepare_ae_specific(meta,
                                population = "apat",
                                observation = "wk12",
                                parameter = "rel")
 tbl <- outdata |>
-  format_ae_specific()
+  format_ae_specific() |>
+  tlf_ae_specific(
+    medra_version = "24.0",
+    source = "Source:  [CDISCpilot: adam-adsl; adae]",
+    path_outdata =  file.path(testthat::test_path(),'fixtures', 'ae0specific1.rdata'),
+    path_outtable =  file.path(testthat::test_path(),'fixtures', 'ae0specific1.rtf')
+  )
 
-tbl |>  tlf_ae_specific(
-  medra_version = "24.0",
-  source = "Source:  [CDISCpilot: adam-adsl; adae]",
-  path_outtable = "outtable/ae0specific1.rtf"
-)
+test_that("tlf_ae_specific() test", {
+  expect_snapshot_file(file.path(testthat::test_path(),'fixtures', 'ae0specific1.rdata'))
+  expect_snapshot_file(file.path(testthat::test_path(),'fixtures', 'ae0specific1.rtf'))
+})
+
