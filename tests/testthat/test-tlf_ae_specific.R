6baa556a72eda8e0a23bde576cd227ff64a64506
Baek, Howard <howard.baek@merck.com>
2022-08-02 19:03:03 -0400

    changed code according to Yilong's suggestion to follow the update in #73 to pass code coverage test

--- tests/testthat/test-tlf_ae_specific.R
+++ tests/testthat/test-tlf_ae_specific.R
@@ -5,17 +5,19 @@ outdata <- prepare_ae_specific(meta,
                                population = "apat",
                                observation = "wk12",
                                parameter = "rel")
+
+path_rtf <- file.path(tempdir(), "ae0specific1.rtf")
+path_rdata <- tempfile(fileext = '.Rdata')
+
 tbl <- outdata |>
   format_ae_specific() |>
   tlf_ae_specific(
     medra_version = "24.0",
     source = "Source:  [CDISCpilot: adam-adsl; adae]",
-    path_outdata =  file.path(testthat::test_path(),'fixtures', 'ae0specific1.rdata'),
-    path_outtable =  file.path(testthat::test_path(),'fixtures', 'ae0specific1.rtf')
+    path_outdata =  path_rdata,
+    path_outtable =  path_rtf
   )
 
-test_that("tlf_ae_specific() test", {
-  expect_snapshot_file(file.path(testthat::test_path(),'fixtures', 'ae0specific1.rdata'))
-  expect_snapshot_file(file.path(testthat::test_path(),'fixtures', 'ae0specific1.rtf'))
+test_that("ae0specific1.rtf file output", {
+  expect_snapshot_file(path_rtf)
 })
-
