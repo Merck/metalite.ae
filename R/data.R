b1f79b904c244a3d1d179bcfecab087630ea30be
chauhaje <jeetender.chauhan@merck.com>
2023-07-14 15:34:16 -0400

    modified document file as it was throwing warning in github due to space in item .

--- R/data.R
+++ R/data.R
@@ -2,17 +2,16 @@
 #'
 #' A dataset containing exposure detail.
 #'
-#' @format A data frame with 280901 rows and 4 variables:
+#' @format A data frame with 591 rows and 41 variables:
 #' \describe{
-#' \item{ASTDT}{Analysis Start Date.}
-#' \item{AENDT}{Analysis End Date.}
-#' \item{ASTDTM}{Analysis Start Date Time.}
-#' \item{AENDTM}{Analysis End Date Time.}
-#' \item{ASTDY}{Analysis Study Day.}
-#' \item{AENDY}{Exposure Duration.}
-#' \item{EXDURDD}{Analysis Start Date.}
-#' \item{EXNUMDOS}{Number of Daily Doses.}
+#'   \item{ASTDT}{Analysis Start Date.}
+#'   \item{AENDT}{Analysis End Date.}
+#'   \item{ASTDTM}{Analysis Start Date Time.}
+#'   \item{AENDTM}{Analysis End Date Time.}
+#'   \item{ASTDY}{Analysis Study Day.}
+#'   \item{AENDY}{Exposure Duration.}
+#'   \item{EXDURDD}{Analysis Start Date.}
+#'   \item{EXNUMDOS}{Number of Daily Doses.}
 #' }
-#' @source \url{https://github.com/phuse-org/phuse-scripts/tree/master
-#/data/sdtm/cdiscpilot01}
+#' @source \url{https://github.com/phuse-org/phuse-scripts/tree/master/data/sdtm/cdiscpilot01}
 "metalite.ae_adex"
