261ea6b526d7934832793c9c62af680f51d37be1
ginnaram <madhusudhan.ginnaram.reddy@merck.com>
2023-10-02 09:24:59 -0400

    Tlf_ae_specific_subgroup testing completed

---
+++ tests/testthat/test-independent-testing-tlf_ae_specific_subgroup.R
@@ -0,0 +1,28 @@
+
+
+library(metalite)
+meta <- meta_ae_example()
+outdata <- prepare_ae_specific_subgroup(meta,
+                                    population = "apat",
+                                    observation = "wk12",
+                                    parameter = "rel",
+                                    subgroup_var = "SEX")
+
+test_that("rtf output: n, and prop w/o total", {
+  path_rtf <- file.path(tempdir(), "ednp_ae0specific1.rtf")
+  path_rdata <- tempfile(fileext = ".Rdata")
+
+  tbl <- outdata |>
+    # extend_ae_specific_events() |>
+    # extend_ae_specific_duration(duration_var = "ADURN") |>
+    format_ae_specific_subgroup(
+    ) |>
+    tlf_ae_specific_subgroup(
+      meddra_version = "24.0",
+      source = "Source:  [CDISCpilot: adam-adsl; adae]",
+      # path_outdata = path_rdata,
+      path_outtable = path_rtf
+    )
+
+  testthat::expect_snapshot_file(path_rtf)
+})
