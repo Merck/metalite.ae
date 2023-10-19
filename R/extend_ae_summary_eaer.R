6389fdf3881503acf2e6861011be428fb414cb18
wangben718 <wangben718@users.noreply.github.com>
2023-10-19 18:00:34 +0000

    Style code (GHA)

--- R/extend_ae_summary_eaer.R
+++ R/extend_ae_summary_eaer.R
@@ -1,7 +1,7 @@
 extend_ae_summary_eaer <- function(outdata,
-                               duration_var = "TRTDUR",
-                               adj_unit = c("year", "month", "week", "day"),
-                               ...) {
+                                   duration_var = "TRTDUR",
+                                   adj_unit = c("year", "month", "week", "day"),
+                                   ...) {
   time_unit <- list("year" = 365.24, "month" = 30.4367, "week" = 7, "day" = 1)
   adj_unit <- match.arg(adj_unit)
   exp_factor <- 100 * time_unit[[adj_unit]]
@@ -27,16 +27,16 @@ extend_ae_summary_eaer <- function(outdata,
   adj_rate_table <- do.call(rbind, res)
 
   metalite::outdata(meta, population, observation, parameter,
-                    n = rbind(n_pop, tbl_num),
-                    order = c(1, seq_len(nrow(tbl_num)) * 100),
-                    group = res[[1]]$group,
-                    reference_group = res[[1]]$reference_group,
-                    prop = rbind(pop_prop, tbl_prop),
-                    diff = rbind(pop_diff, tbl_diff),
-                    n_pop = n_pop,
-                    name = c(pop_name, name),
-                    prepare_call = match.call(),
-                    eaer = eaer
+    n = rbind(n_pop, tbl_num),
+    order = c(1, seq_len(nrow(tbl_num)) * 100),
+    group = res[[1]]$group,
+    reference_group = res[[1]]$reference_group,
+    prop = rbind(pop_prop, tbl_prop),
+    diff = rbind(pop_diff, tbl_diff),
+    n_pop = n_pop,
+    name = c(pop_name, name),
+    prepare_call = match.call(),
+    eaer = eaer
   )
 }
 
@@ -57,4 +57,3 @@ f_nae <- function(x, meta, observation) {
   }
   return(num)
 }
-
