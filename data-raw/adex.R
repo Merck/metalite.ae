aab1fac2f8f5e09ef6dba2c770e647581bc10921
Nan Xiao <me@nanx.me>
2024-04-30 17:17:57 -0400

    Qualify namespaces explicitly

--- data-raw/adex.R
+++ data-raw/adex.R
@@ -1,15 +1,10 @@
-library(haven)
-library(dplyr)
-library(r2rtf)
-library(Hmisc)
-
 # Read in the ADSL and EX datasets
-ex0 <- read_xpt("data-raw/ex.xpt")
+ex0 <- haven::read_xpt("data-raw/ex.xpt")
 write.csv(ex0, file = "data-raw/ex.csv", row.names = FALSE, quote = FALSE)
 
 # Create a new data object with selected variables
 adsl <- r2rtf::r2rtf_adsl
-adsl <- select(
+adsl <- dplyr::select(
   adsl, STUDYID, SITEID, USUBJID, starts_with("TRT01"), AGE,
   starts_with("AGEGR1"),
   starts_with("RACE"), SEX, SAFFL, ITTFL, TRTSDT, TRTEDT
@@ -20,8 +15,8 @@ ex1 <- merge(adsl, ex0, by = c("STUDYID", "USUBJID"))
 
 # Derive ASTDT and AENDT
 adex1 <- ex1 |>
-  mutate(ASTDT = as.Date(EXSTDTC, format = "%Y-%m-%d")) |>
-  mutate(AENDT = as.Date(EXENDTC, format = "%Y-%m-%d"))
+  dplyr::mutate(ASTDT = as.Date(EXSTDTC, format = "%Y-%m-%d")) |>
+  dplyr::mutate(AENDT = as.Date(EXENDTC, format = "%Y-%m-%d"))
 
 # ASTDTM and AENDTM
 adex1$ASTDTM <- ifelse(
@@ -38,30 +33,30 @@ adex1$AENDTM <- ifelse(
 
 # Derive ASTDY AENDY EXDURDD EXDURDDU
 adex2 <- adex1 |>
-  mutate(ASTDY = ASTDT - TRTSDT + (ASTDT >= TRTSDT)) |>
-  mutate(AENDY = AENDT - TRTSDT + (AENDT >= TRTSDT)) |>
-  mutate(EXDURDD = AENDT - ASTDT + (AENDT >= ASTDT)) |>
-  mutate(EXDURDDU = "Days")
+  dplyr::mutate(ASTDY = ASTDT - TRTSDT + (ASTDT >= TRTSDT)) |>
+  dplyr::mutate(AENDY = AENDT - TRTSDT + (AENDT >= TRTSDT)) |>
+  dplyr::mutate(EXDURDD = AENDT - ASTDT + (AENDT >= ASTDT)) |>
+  dplyr::mutate(EXDURDDU = "Days")
 
 # Convert ASTDY AENDY and EXDURDD into numeric
 adex3 <- adex2 |>
-  mutate(ASTDY = as.numeric(adex2$ASTDY)) |>
-  mutate(AENDY = as.numeric(adex2$AENDY)) |>
-  mutate(EXDURDD = as.numeric(adex2$EXDURDD))
+  dplyr::mutate(ASTDY = as.numeric(adex2$ASTDY)) |>
+  dplyr::mutate(AENDY = as.numeric(adex2$AENDY)) |>
+  dplyr::mutate(EXDURDD = as.numeric(adex2$EXDURDD))
 
 # Derive EXNUMDOS: If EXDOSE ge 0 then 1 else 0
 adex3$EXNUMDOS <- ifelse(adex3$EXDOSE >= 0, 1, 0)
 
 # Assign Label to derived variables
-label(adex3$ASTDT) <- "Analysis Start Date"
-label(adex3$AENDT) <- "Analysis End Date"
-label(adex3$ASTDTM) <- "Analysis Start Date Time"
-label(adex3$AENDTM) <- "Analysis End Date Time"
-label(adex3$ASTDY) <- "Analysis Study Day"
-label(adex3$AENDY) <- "Analysis End Day"
-label(adex3$EXDURDD) <- "Exposure Duration"
-label(adex3$EXDURDDU) <- "Exposure Duration Unit"
-label(adex3$EXNUMDOS) <- "Number of Daily Doses"
+Hmisc::label(adex3$ASTDT) <- "Analysis Start Date"
+Hmisc::label(adex3$AENDT) <- "Analysis End Date"
+Hmisc::label(adex3$ASTDTM) <- "Analysis Start Date Time"
+Hmisc::label(adex3$AENDTM) <- "Analysis End Date Time"
+Hmisc::label(adex3$ASTDY) <- "Analysis Study Day"
+Hmisc::label(adex3$AENDY) <- "Analysis End Day"
+Hmisc::label(adex3$EXDURDD) <- "Exposure Duration"
+Hmisc::label(adex3$EXDURDDU) <- "Exposure Duration Unit"
+Hmisc::label(adex3$EXNUMDOS) <- "Number of Daily Doses"
 
 metalite_ae_adex <- adex3
 
--- data-raw/adexsum.R
+++ data-raw/adexsum.R
@@ -1,27 +1,25 @@
-library(haven)
-library(dplyr)
-library(r2rtf)
-library(Hmisc)
-
 # Read in ADEX dataset
-load("~/metalite.ae1/data/metalite_ae_adex.rda")
+load("data/metalite_ae_adex.rda")
 
 adex <- metalite_ae_adex
 
-adex1 <- adex %>%
-  group_by(USUBJID) %>%
-  mutate(
+adex1 <- adex |>
+  dplyr::group_by(USUBJID) |>
+  dplyr::mutate(
     AVAL = sum(EXDURDD, na.rm = TRUE),
     PARAM = "Duration of Exposure(days)",
     PARAMCD = "EXPODUR"
-  ) %>%
-  select(-VISIT, -VISITDY, -VISITNUM, -EXSTDTC, -EXENDTC, -EXENDY, -EXSTDY, -ASTDT, -AENDT, -ASTDTM, -AENDTM, -ASTDY, -AENDY, -EXDURDD) %>%
-  distinct(USUBJID, .keep_all = TRUE)
+  ) |>
+  dplyr::select(
+    -VISIT, -VISITDY, -VISITNUM, -EXSTDTC, -EXENDTC, -EXENDY, -EXSTDY,
+    -ASTDT, -AENDT, -ASTDTM, -AENDTM, -ASTDY, -AENDY, -EXDURDD
+  ) |>
+  dplyr::distinct(USUBJID, .keep_all = TRUE)
 
 # Assign Label to derived variables
-label(adex1$AVAL) <- "Analysis Value"
-label(adex1$PARAM) <- "Parameter"
-label(adex1$PARAMCD) <- "Parameter Code"
+Hmisc::label(adex1$AVAL) <- "Analysis Value"
+Hmisc::label(adex1$PARAM) <- "Parameter"
+Hmisc::label(adex1$PARAMCD) <- "Parameter Code"
 
 metalite_ae_adexsum <- adex1
 
