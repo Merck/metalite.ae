059b0343d86d558564f2b2780d7c6acb7f18deba
Chen Wang <chen.wang16@merck.com>
2025-01-31 13:27:41 -0500

    Assign value for AEACN

--- R/meta_ae_example.R
+++ R/meta_ae_example.R
@@ -70,18 +70,21 @@ meta_ae_example <- function() {
   }
 
   # AE action
-  adae$AEACN <- gsub("", "DOSE NOT CHANGED", adae$AEACN)
+  adae$AEACN <- sample(
+    x = c("DOSE NOT CHANGED", "DRUG INTERRUPTED", "DRUG WITHDRAWN", "NOT APPLICABLE", "UNKNOWN"),
+    size = length(adae$USUBJID),
+    prob = c(0.7, 0.1,0.05,0.1,0.05), replace = TRUE
+  )
 
   for (i in seq_along(adae$AEACN)) {
-    adae$action_taken[i] <- switch(adae$AEACN[i],
-      "DOSE NOT CHANGED" = "None",
-      "DOSE REDUCED" = "Reduced",
-      "DRUG INTERRUPTED" = "Interrupted",
-      "DOSE INCREASED" = "Increased",
-      "NOT APPLICABLE" = "N/A",
-      "UNKNOWN" = "Unknown",
-      "''" = "None",
-      tools::toTitleCase(tolower(adae$AEACN[i]))
+  adae$action_taken[i] <- switch(adae$AEACN[i],
+                                  "DOSE NOT CHANGED" = "None",
+                                  "DRUG INTERRUPTED" = "Interrupted",
+                                  "DRUG WITHDRAWN" = "Discontinued",
+                                  "NOT APPLICABLE" = "N/A",
+                                  "UNKNOWN" = "Unknown",
+                                  "''" = "None",
+                                  tools::toTitleCase(tolower(adae$AEACN[i]))
     )
   }
 
