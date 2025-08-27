19acf794cbc398f620e64f7e173109a0c8b98e58
ginnaram13 <ginnaram13@users.noreply.github.com>
2025-08-27 18:26:35 +0000

    Style code (GHA)

--- R/gt_ae_summary.R
+++ R/gt_ae_summary.R
@@ -35,20 +35,21 @@
 #' )
 #' outdata |>
 #'   format_ae_summary() |>
-#'   gt_ae_summary(analysis ="ae_summary",
-#'   footnotes = "Test",
-#'   source = "Source:  [CDISCpilot: adam-adsl; adae]")
-
+#'   gt_ae_summary(
+#'     analysis = "ae_summary",
+#'     footnotes = "Test",
+#'     source = "Source:  [CDISCpilot: adam-adsl; adae]"
+#'   )
 gt_ae_summary <- function(outdata,
-                           source,
-                           analysis,
-                           col_rel_width = NULL,
-                           text_font_size = 9,
-                           orientation = "portrait",
-                           title = c("analysis", "observation", "population"),
-                           footnotes = NULL,
-                           path_outdata = NULL,
-                           path_outtable = NULL) {
+                          source,
+                          analysis,
+                          col_rel_width = NULL,
+                          text_font_size = 9,
+                          orientation = "portrait",
+                          title = c("analysis", "observation", "population"),
+                          footnotes = NULL,
+                          path_outdata = NULL,
+                          path_outtable = NULL) {
   tbl <- outdata$tbl
   group <- outdata$group
   num_groups <- length(group)
@@ -56,8 +57,8 @@ gt_ae_summary <- function(outdata,
   n_group <- length(outdata$group)
   n_row <- nrow(tbl)
   n_col <- ncol(tbl)
- # analysis <- "ae_summary"
- # source = "Source:  [CDISCpilot: adam-adsl; adae]"
+  # analysis <- "ae_summary"
+  # source = "Source:  [CDISCpilot: adam-adsl; adae]"
 
   # Check if the parameter analysis contains the correct analysis that should exist in "outdata$meta$analysis"
   analysis_name <- names(outdata$meta$analysis)
@@ -74,10 +75,10 @@ gt_ae_summary <- function(outdata,
   # Define title
   if ("analysis" %in% title | "observation" %in% title | "population" %in% title) {
     title <- collect_title(outdata$meta,
-                           outdata$population,
-                           outdata$observation,
-                           "any",
-                           analysis = "ae_summary"
+      outdata$population,
+      outdata$observation,
+      "any",
+      analysis = "ae_summary"
     )
     print(title)
   }
@@ -87,51 +88,50 @@ gt_ae_summary <- function(outdata,
     collect_adam_mapping(outdata$meta, x)$summ_foot
   })
   footnotes <- c(unlist(x), footnotes)
-  #footnotes <- paste(footnotes, collapse = "  \n")
-   tbl
-    combined_title_md <- paste(title, collapse = "  \n")
+  # footnotes <- paste(footnotes, collapse = "  \n")
+  tbl
+  combined_title_md <- paste(title, collapse = "  \n")
 
-    # Function to create dynamic cols_label named vector
-    create_cols_label <- function(num_groups) {
-      n_labels <- setNames(rep("n", num_groups), paste0("n_", seq_len(num_groups)))
-      prop_labels <- setNames(rep("(%)", num_groups), paste0("prop_", seq_len(num_groups)))
-      c(name = "", n_labels, prop_labels)
-    }
+  # Function to create dynamic cols_label named vector
+  create_cols_label <- function(num_groups) {
+    n_labels <- setNames(rep("n", num_groups), paste0("n_", seq_len(num_groups)))
+    prop_labels <- setNames(rep("(%)", num_groups), paste0("prop_", seq_len(num_groups)))
+    c(name = "", n_labels, prop_labels)
+  }
 
-    # Create spanner functions list
-    spanner_funs <- lapply(seq_along(group), function(i) {
-      function(gt_tbl) {
-        gt_tbl %>% tab_spanner(
-          label = group[i],
-          columns = c(paste0("n_", i), paste0("prop_", i))
-        )
-      }
-    })
-
-      n_labels <- setNames(rep("n", num_groups), paste0("n_", seq_len(num_groups)))
-      prop_labels <- setNames(rep("(%)", num_groups), paste0("prop_", seq_len(num_groups)))
-
-      name_col <- colnames(tbl)[1]  # first column, e.g. "name"
-      n_cols <- paste0("n_", 1:num_groups)
-      prop_cols <- paste0("prop_", 1:num_groups)
-
-      cols_label_vec <- c(
-        setNames("", name_col),          # first column label is empty string
-        setNames(rep("n", num_groups), n_cols),
-        setNames(rep("(%)", num_groups), prop_cols)
+  # Create spanner functions list
+  spanner_funs <- lapply(seq_along(group), function(i) {
+    function(gt_tbl) {
+      gt_tbl %>% tab_spanner(
+        label = group[i],
+        columns = c(paste0("n_", i), paste0("prop_", i))
       )
+    }
+  })
+
+  n_labels <- setNames(rep("n", num_groups), paste0("n_", seq_len(num_groups)))
+  prop_labels <- setNames(rep("(%)", num_groups), paste0("prop_", seq_len(num_groups)))
+
+  name_col <- colnames(tbl)[1] # first column, e.g. "name"
+  n_cols <- paste0("n_", 1:num_groups)
+  prop_cols <- paste0("prop_", 1:num_groups)
+
+  cols_label_vec <- c(
+    setNames("", name_col), # first column label is empty string
+    setNames(rep("n", num_groups), n_cols),
+    setNames(rep("(%)", num_groups), prop_cols)
+  )
 
 
-      gt_tbl <- tbl |>
-      gt() |>
-      sub_missing(columns = 1:ncol(tbl), missing_text = "") |>
-      fmt_markdown(columns = 1) |>
-      tab_header(title = gt::md(combined_title_md)) |>
-      (\(gt_tbl) Reduce(function(acc, f) f(acc), spanner_funs, init = gt_tbl))() |>
-      cols_label(!!!cols_label_vec) |>
-      tab_source_note(footnotes) |>
-      tab_source_note(source)
+  gt_tbl <- tbl |>
+    gt() |>
+    sub_missing(columns = 1:ncol(tbl), missing_text = "") |>
+    fmt_markdown(columns = 1) |>
+    tab_header(title = gt::md(combined_title_md)) |>
+    (\(gt_tbl) Reduce(function(acc, f) f(acc), spanner_funs, init = gt_tbl))() |>
+    cols_label(!!!cols_label_vec) |>
+    tab_source_note(footnotes) |>
+    tab_source_note(source)
 
-      gtsave(gt_tbl, filename = "~/my_table.png")
-
+  gtsave(gt_tbl, filename = "~/my_table.png")
 }
