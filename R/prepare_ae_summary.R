c8299a59d7067768f0fd788af64063b7837a5cae
LittleBeannie <yujie.zhao@merck.com>
2026-07-27 16:55:24 -0400

    Update `format_ae_summary`

--- R/prepare_ae_summary.R
+++ R/prepare_ae_summary.R
@@ -126,4 +126,51 @@ prepare_ae_summary <- function(meta,
 #' tbl <- outdata |>
 #'   format_ae_summary()
 #' head(tbl$tbl)
-format_ae_summary <- format_ae_specific
+format_ae_summary <- function(outdata,
+                              display = c("n", "prop", "total"),
+                              hide_soc_stats = FALSE,
+                              digits_prop = 1,
+                              digits_ci = 1,
+                              digits_p = 3,
+                              digits_dur = c(1, 1),
+                              digits_events = c(1, 1),
+                              filter_method = c("percent", "count"),
+                              filter_criteria = 0,
+                              sort_order = c("alphabetical", "count_des", "count_asc"),
+                              sort_column = NULL,
+                              mock = FALSE) {
+  if (length(outdata$name) != nrow(outdata$n)) {
+    parameters <- unlist(strsplit(outdata$parameter, ";"))
+    summary_names <- vapply(parameters, function(parameter) {
+      mapping <- collect_adam_mapping(outdata$meta, parameter)
+
+      if (is.null(mapping$summ_row)) mapping$label else mapping$summ_row
+    }, character(1))
+
+    if ("any" %in% parameters) {
+      summary_names <- append(
+        summary_names,
+        "with no adverse events",
+        after = 1
+      )
+    }
+
+    outdata$name <- c(outdata$name[1], summary_names)
+  }
+
+  format_ae_specific(
+    outdata = outdata,
+    display = display,
+    hide_soc_stats = hide_soc_stats,
+    digits_prop = digits_prop,
+    digits_ci = digits_ci,
+    digits_p = digits_p,
+    digits_dur = digits_dur,
+    digits_events = digits_events,
+    filter_method = filter_method,
+    filter_criteria = filter_criteria,
+    sort_order = sort_order,
+    sort_column = sort_column,
+    mock = mock
+  )
+}
